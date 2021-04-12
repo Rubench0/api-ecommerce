9442fc10a47e8e756175d66ae0704424a8652796
Ruben Garcia <rubenchoo.garcia@gmail.com>
2021-04-12 18:49:23 -0500

    implementado el jwt para la proteccion de rutas

---
+++ app/Http/Middleware/JwtMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use JWTAuth;
+use Exception;
+use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
+
+class JwtMiddleware extends BaseMiddleware
+{
+
+    public function handle($request, Closure $next)
+    {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (Exception $e) {
+            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
+                return response()->json(['status' => 'El token es invalido']);
+            } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
+                return response()->json(['status' => 'El token ha expirado']);
+            } else {
+                return response()->json(['status' => 'Token de autorización no encontrado']);
+            }
+        }
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('register', 'App\Http\Controllers\AuthController@signUp');
+Route::get('login', 'App\Http\Controllers\AuthController@auth');
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::get('user','App\Http\Controllers\AuthController@getAuthUser');
 });
